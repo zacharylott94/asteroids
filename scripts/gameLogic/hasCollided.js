9d42f07c599a3d9c9cc2fb88a78693a76a7e9333
Zach Lott <zacharylott94@gmail.com>
2020-06-14 19:01:48 -0500

    UUIDs are not needed to check if objects are equal

--- scripts/gameLogic/hasCollided.js
+++ scripts/gameLogic/hasCollided.js
@@ -1,10 +1,6 @@
 import Vector from "../objects/Vector.js";
-import ObjectPool from "./ObjectPool.js";
 const hasCollided = (obj, obj2) => {
-    //hash the objects to see if they are the same object
-    const hash1 = ObjectPool.id(obj);
-    const hash2 = ObjectPool.id(obj2);
-    if (hash1 === hash2)
+    if (obj === obj2)
         return false;
     const squaredDistanceBetweenObjects = Vector.distanceSquared(obj.position, obj2.position);
     const summedRadiiOfObjects = obj.radius + obj2.radius;
