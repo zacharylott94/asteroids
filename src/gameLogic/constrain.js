ca8913fdef8cd69ca8dc776dbef6d31fb5e6b29a
Zach Lott <zacharylott94@gmail.com>
2020-12-14 13:58:23 -0600

    Trying to reduce Constrain's Canvas dependency

--- src/gameLogic/constrain.js
+++ src/gameLogic/constrain.js
@@ -5,19 +5,23 @@ const object = (object) => {
     object.position = Constrain.vector(object.position)
 };
 
-const vector = (vector) => {
+const vector = (vector, gameWidth, gameHeight) => {
+    //Backwards compatibility
+    gameWidth = gameWidth === undefined? Canvas.width : gameWidth
+    gameHeight = gameHeight === undefined? Canvas.height : gameHeight
+
     let result = new Vector(vector.x,vector.y)
-    if (vector.x > Canvas.width) {                   //if object is past the right edge of the screen
-        result.x = vector.x - Canvas.width; //subtract the width of the screen to wrap it to the left
+    if (vector.x > gameWidth) {                   //if object is past the right edge of the screen
+        result.x = vector.x - gameWidth; //subtract the width of the screen to wrap it to the left
     }
-    if (vector.y > Canvas.height) {                   //if object is past the bottom edge of the screen
-        result.y = vector.y - Canvas.height; //subtract the height of the screen to wrap it to the top
+    if (vector.y > gameHeight) {                   //if object is past the bottom edge of the screen
+        result.y = vector.y - gameHeight; //subtract the height of the screen to wrap it to the top
     }
     if (vector.x < 0) {                           //if the object is past the left edge of the screen
-        result.x = vector.x + Canvas.width; //add the width of the screen to wrap it to the right
+        result.x = vector.x + gameWidth; //add the width of the screen to wrap it to the right
     }
     if (vector.y < 0) {                            //if the object is past the top of the screen
-        result.y = vector.y + Canvas.height; //add the height of the screen to wrap it to the bottom
+        result.y = vector.y + gameHeight; //add the height of the screen to wrap it to the bottom
     }
     return result
 }
