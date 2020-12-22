d7af45a1577827175a211fa276d9b455ba80dcfa
Zach Lott <zacharylott94@gmail.com>
2020-12-22 17:34:58 -0600

    Named some values in Projectile class

--- src/objects/Projectile.js
+++ src/objects/Projectile.js
@@ -1,17 +1,20 @@
 import GameObject from "./GameObject.js"
 import Circle from "./Circle.js"
 
+const speed = 5
+const size = 5
+const timeToLive = 200 //in physics frames
 class Projectile extends GameObject {
   constructor(position, rotation) {
-    super(position, rotation.scale(10), Circle, 5)
-    this.timeToLive = 200
+    super(position, rotation.scale(speed), Circle, size)
+    this.timeToLive = timeToLive
   }
   update () {
     super.update()
     if (this.timeToLive < 1) this.delete()
     this.timeToLive--
   }
-  
+
   handleCollision(obj){
     super.handleCollision(obj)
     if (obj.constructor.name === "Asteroid") this.delete()
