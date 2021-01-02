4b26a011b329119909d79b079b523d1b90ed011a
Zach Lott <zacharylott94@gmail.com>
2021-01-02 15:46:05 -0600

    ObjectPool methods references itself by 'this'

--- src/gameLogic/ObjectPool.js
+++ src/gameLogic/ObjectPool.js
@@ -2,7 +2,7 @@ const ObjectPool = new Set()
 
 ObjectPool.count = function(type) {
   let count = 0
-  for (const each of ObjectPool.values()){
+  for (const each of this.values()){
     if (each.constructor.name === type)
       count++
   }
@@ -11,14 +11,14 @@ ObjectPool.count = function(type) {
 
 //Returns first object only. Used to get Player
 ObjectPool.getPlayer = function() {
-  for (const each of ObjectPool.values()){
+  for (const each of this.values()){
     if (each.constructor.name === "Player")
       return each
   }
 }
 
 ObjectPool.reset = function() {
-  for (const each of ObjectPool.values()) ObjectPool.delete(each)
+  for (const each of this.values()) this.delete(each)
 }
 
 export default ObjectPool
