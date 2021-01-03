2b9022b0516fc982969dceb30f1c38dfa08e0cd9
Zach Lott <zacharylott94@gmail.com>
2021-01-02 19:48:45 -0600

    Changed Constrain to a static class

--- src/gameLogic/constrain.js
+++ src/gameLogic/constrain.js
@@ -1,29 +1,28 @@
 import Vector from "../objects/Vector.js"
 //makes sure the object stays in the playing field
-const object = (object, width, height) => {
-    object.position = Constrain.vector(object.position, width, height)
-};
 
-const vector = (vector, gameWidth, gameHeight) => {
+class Constrain {
+    static object(object, width, height) {
+        object.position = Constrain.vector(object.position, width, height)
+    }
 
-    let result = new Vector(vector.x,vector.y)
-    if (vector.x > gameWidth) {                   //if object is past the right edge of the screen
-        result.x = vector.x - gameWidth; //subtract the width of the screen to wrap it to the left
+    static vector(vector, gameWidth, gameHeight) {
+
+        let result = new Vector(vector.x,vector.y)
+        if (vector.x > gameWidth) {                   //if object is past the right edge of the screen
+            result.x = vector.x - gameWidth; //subtract the width of the screen to wrap it to the left
+        }
+        if (vector.y > gameHeight) {                   //if object is past the bottom edge of the screen
+            result.y = vector.y - gameHeight; //subtract the height of the screen to wrap it to the top
+        }
+        if (vector.x < 0) {                           //if the object is past the left edge of the screen
+            result.x = vector.x + gameWidth; //add the width of the screen to wrap it to the right
+        }
+        if (vector.y < 0) {                            //if the object is past the top of the screen
+            result.y = vector.y + gameHeight; //add the height of the screen to wrap it to the bottom
+        }
+        return result
     }
-    if (vector.y > gameHeight) {                   //if object is past the bottom edge of the screen
-        result.y = vector.y - gameHeight; //subtract the height of the screen to wrap it to the top
-    }
-    if (vector.x < 0) {                           //if the object is past the left edge of the screen
-        result.x = vector.x + gameWidth; //add the width of the screen to wrap it to the right
-    }
-    if (vector.y < 0) {                            //if the object is past the top of the screen
-        result.y = vector.y + gameHeight; //add the height of the screen to wrap it to the bottom
-    }
-    return result
-}
-const Constrain = {
-    object,
-    vector
 }
 
 export default Constrain
