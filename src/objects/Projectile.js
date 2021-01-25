ecf1a119d6207e56df22809126ca0582aa8c44e7
Zach Lott <zacharylott94@gmail.com>
2021-01-25 05:49:54 -0600

    Refactored Projectile into Factory

--- src/objects/Projectile.js
+++ src/objects/Projectile.js
@@ -1,39 +1,18 @@
-import GameObject from "./GameObject.js"
 import EventCoordinator from "./EventCoordinator.js"
 import Settings from "../gameLogic/Settings.js"
 import diamond from "../draw/Diamond.js";
 import Sound from "../gameLogic/Sound.js";
 import Random from "../gameLogic/random.js";
-import RenderComponent from "./components/renderComponent.js";
-import ColliderComponent from "./components/colliderComponent.js"
 import ObjectList from "../gameLogic/ObjectList.js";
+import { canMove } from "./behaviors/canMove.js"
+import { canRender } from "./canRender.js";
 
 const shootSounds = [
   new Sound("/asteroids/src/sfx/shoot.wav"),
   new Sound("/asteroids/src/sfx/shoot2.wav"),
   new Sound("/asteroids/src/sfx/shoot3.wav"),
 ]
-class Projectile extends GameObject {
-  constructor(position, rotationVector) {
-    super(position, rotationVector.scale(Settings.PROJECTILE_SPEED), Settings.PROJECTILE_SIZE)
-    this.timeToLive = Settings.PROJECTILE_TIME_TO_LIVE
-    this.rotation = rotationVector.degrees()
-    this.shootSound = new Sound(shootSounds[Random.int(2)].getSrc())
-    this.shootSound.play()
-    this.renderComponent = new RenderComponent(diamond, this)
-    this.collider = new ColliderComponent(this)
-
-    Object.assign(
-      this,
-      canDelete(this)
-    )
-  }
-  update () {
-    super.update()
-    if (this.timeToLive < 1) this.delete()
-    this.timeToLive--
-  }
-
+class Projectile{
   handleCollision(obj){
     if(!this.collider.collidedWith(obj)) return
     if (obj.constructor.name === "Asteroid") {
@@ -41,9 +20,7 @@ class Projectile extends GameObject {
       this.delete()
     }
   }
-
-
-  }
+}
 
 const canDelete = projectile => {
   const deleteThis = _ => {
@@ -52,6 +29,40 @@ const canDelete = projectile => {
   }
   return {delete:deleteThis}
 }
+
+const canUpdate = (projectile, projectileData) => {
+  const update = _ => {
+    projectile.move()
+    if (projectileData.timeToLive < 1) projectile.delete()
+    projectileData.timeToLive--
+  }
+  return {update}
 }
 
-export default Projectile
+const ProjectileFactory = (position, rotationVector) => {
+  let projectileData = {
+    position,
+    velocity: rotationVector.scale(Settings.PROJECTILE_SPEED),
+    radius: Settings.PROJECTILE_SIZE,
+    timeToLive: Settings.PROJECTILE_TIME_TO_LIVE,
+    rotation: rotationVector.degrees(),
+    // renderComponent: new RenderComponent(diamond, this),
+    // collider: new ColliderComponent(this),
+  }
+
+  let projectile = {}
+  Object.assign(
+    projectile,
+    canRender(projectileData, diamond),
+    canUpdate(projectile, projectileData),
+    canDelete(projectile),
+    canMove(projectileData),
+
+
+  )
+  shootSounds[Random.int(2)].play()
+  ObjectList.add(projectile)
+  return projectile
+}
+
+export default ProjectileFactory
--- src/objects/behaviors/canFireProjectile.js
+++ src/objects/behaviors/canFireProjectile.js
@@ -1,10 +1,10 @@
-import Projectile from "../Projectile.js";
+import ProjectileFactory from "../Projectile.js";
 import Vector from "../vector/Vector.js";
 
 export const canFireProjectile = (object) => {
     const fireProjectile = () => {
         if (object.activeProjectiles.size < 3) {
-            object.activeProjectiles.add(new Projectile(object.position, Vector.fromDegreesAndMagnitude(object.rotation, 1)));
+            object.activeProjectiles.add(ProjectileFactory(object.position, Vector.fromDegreesAndMagnitude(object.rotation, 1)));
         }
     };
     const decrementActiveProjectiles = ([projectile]) => {
