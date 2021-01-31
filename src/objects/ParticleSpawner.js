1b92bf9fa68855888d31f8169122539768fd189b
Zach Lott <zacharylott94@gmail.com>
2021-01-31 16:02:52 -0600

    Cleaned up ParticleSpawner some

--- src/objects/ParticleSpawner.js
+++ src/objects/ParticleSpawner.js
@@ -1,7 +1,6 @@
 import Circle from "../draw/Circle.js";
 import { randomAngle } from "../gameLogic/random.js";
 import { Particle } from "./Particle.js";
-import Position from "./vector/Position.js";
 import Vector from "./vector/Vector.js"
 
 const defaults = {
@@ -15,52 +14,52 @@ const defaults = {
 
 export const ParticleSpawnerBuilder = _ => {
   let builder = {spawner: {...defaults}}
+  let spawner = {...defaults}
   const build = _ => {
     return {emit}
   }
   const withSpread = spread => {
-    builder.spawner.spread = spread
+    spawner.spread = spread
     return builder
   }
   const withDensity = density => {
-    builder.spawner.density = density
+    spawner.density = density
     return builder
   }
   const withDraw = drawFunction => {
-    builder.spawner.draw = drawFunction
+    spawner.draw = drawFunction
     return builder
   }
   const withParticleTTL = ttl => {
-    builder.spawner.ttl = ttl
+    spawner.ttl = ttl
     return builder
   }
 
   const atAngle = angle => {
-    builder.spawner.angle = angle
+    spawner.angle = angle
     return builder
   }
 
   const withSpeed = speed => {
-    builder.spawner.speed = speed
+    spawner.speed = speed
   }
   Object.assign(
     builder,
-    {build,
-    withSpread,
-    withDensity,
-    withDraw,
-    withParticleTTL,
-    atAngle,
-    withSpeed,
-  }
+    {
+      build,
+      withSpread,
+      withDensity,
+      withDraw,
+      withParticleTTL,
+      atAngle,
+      withSpeed,
+    }
   )
 
   const emit = position => {
-    // console.log(builder.spawner)
-    // let velocity = Vector.fromDegreesAndMagnitude(builder.spawner.angle)
-    for (let i = 0; i < builder.spawner.density; i++) {
-      let velocity = Vector.fromDegreesAndMagnitude(randomAngle(builder.spawner.angle, builder.spawner.spread), Math.random() * builder.spawner.speed)
-      Particle({...builder.spawner, velocity, position})
+    for (let i = 0; i < spawner.density; i++) {
+      let velocity = Vector.fromDegreesAndMagnitude(randomAngle(spawner.angle, spawner.spread), Math.random() * spawner.speed)
+      Particle({...spawner, velocity, position})
     }
   }
 
