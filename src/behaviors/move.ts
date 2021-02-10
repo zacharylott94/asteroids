92359400675b201f0f9b0154153c18fc24927e28
Zach Lott <zacharylott94@gmail.com>
2021-02-10 15:43:32 -0600

    Positions can be constrained to the play field

--- src/behaviors/move.ts
+++ src/behaviors/move.ts
@@ -1,7 +1,7 @@
 import Position from "../gameObjects/Position.js";
 
 export default function (obj: TMoveable): TMoveable {
-  let newPosition = Position.addVector(obj.position, obj.velocity) 
+  let newPosition = Position.constrain(Position.addVector(obj.position, obj.velocity) )
   let newObject: TMoveable = {...obj,position:newPosition}
   return newObject
 }
--- src/gameObjects/Position.ts
+++ src/gameObjects/Position.ts
@@ -62,4 +62,22 @@ export default class Position {
     return closest
   }
 
+  static constrain(position: TPosition):TPosition {
+    let realPosition = Position.real(position)
+    let resultVector:TVector = {...realPosition}
+    if (realPosition.x > Settings.GAME_WIDTH) {                   //if object is past the right edge of the screen
+        resultVector.x = realPosition.x - Settings.GAME_WIDTH; //subtract the width of the screen to wrap it to the left
+    }
+    if (realPosition.y > Settings.GAME_HEIGHT) {                   //if object is past the bottom edge of the screen
+        resultVector.y = realPosition.y - Settings.GAME_HEIGHT; //subtract the height of the screen to wrap it to the top
+    }
+    if (realPosition.x < 0) {                           //if the object is past the left edge of the screen
+        resultVector.x = realPosition.x + Settings.GAME_WIDTH; //add the width of the screen to wrap it to the right
+    }
+    if (realPosition.y < 0) {                            //if the object is past the top of the screen
+        resultVector.y = realPosition.y + Settings.GAME_HEIGHT; //add the height of the screen to wrap it to the bottom
+    }
+    return Position.fromVector(resultVector)
+  }
+
 }
