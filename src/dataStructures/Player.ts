146fb20fc92272c0034e20b41c569bffc2ec6e41
Zach Lott <zacharylott94@gmail.com>
2021-02-20 13:52:09 -0600

    made player radius a setting

--- src/dataStructures/Player.ts
+++ src/dataStructures/Player.ts
@@ -1,9 +1,10 @@
+import { Settings } from "../settings.js"
 import Position from "./Position.js"
 
 export default function PlayerFactory(location: TVector, velocity: TVector, rotation: Degrees): IPlayer {
   return {
     position: Position.fromVector(location),
-    radius: 10,
+    radius: Settings.PLAYER_RADIUS,
     rotation,
     velocity
   }
