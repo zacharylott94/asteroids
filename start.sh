8fc91a25990c06037bd3378bf4f417c1aefc10a0
Zach Lott <zacharylott94@gmail.com>
2021-05-29 15:19:29 -0500

    update start script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 code .
 firefox "http://127.0.0.1:8080/asteroids" &
-x-terminal-emulator --tab -e "http-server .."
-x-terminal-emulator --tab -e "tsc --watch"
+x-terminal-emulator -e "http-server .." 
+x-terminal-emulator -e "tsc --watch"
+jest --watch --coverage ./build
